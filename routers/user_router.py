2dff8bcf082c56435d6b7306f4064ab702ae1c3d
filipvujic <filip.vujic@qcerris.com>
2023-06-19 16:46:46 +0200

    added user deletion

--- routers/user_router.py
+++ routers/user_router.py
@@ -7,3 +7,7 @@ router = APIRouter(tags=["User"], prefix="/user")
 @router.get("/get-by-id/{id_param}")
 async def get_by_id(id_param: str):
     return await user_service.get_by_id(id_param=id_param)
+
+@router.get("/delete-by-id/{id_param}")
+async def delete_by_id(id_param: str):
+    return await user_service.delete_by_id(id_param=id_param)
--- services/user_service.py
+++ services/user_service.py
@@ -1,5 +1,6 @@
 from models.models import User
 from database.db_util import DbUtil
+from sqlalchemy import delete
 
 
 async def get_by_id(id_param: str):
@@ -12,3 +13,15 @@ async def get_by_id(id_param: str):
     
     #return {"message": user1.to_string()}
     return {"message": user2.to_string()}
+
+async def delete_by_id(id_param: str):
+    session = DbUtil.get_session()
+
+    ### Using filter.
+    user1 = session.query(User).filter(User.id == id_param).all()[0]
+
+    statement = delete(User).where(User.id == user1.id)
+    session.execute(statement=statement)
+    session.commit()
+    
+    return {"message": f"Deleted user: {user1.to_string()}" }
